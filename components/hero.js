1765cd897eec7f6c63d358b8ffc545c4ac35f5b8
Reza Ataeigharacheh <bytebridgedev@outlook.com>
2024-04-10 22:39:26 +0100

    connect api to convertKit / field validation

--- components/hero.js
+++ components/hero.js
@@ -1,26 +1,41 @@
 "use client"
 
-import axios from 'axios';
 import React, { useState } from 'react'
 
 export default function HeroSection(){
     const MAILCHIMP_URL = process.env.NEXT_PUBLIC_MAILCHIMP_URL;
-    const [email, setEmail] = useState('')
     const [message, setMessage] = useState('IDLE')
     const [error, setError] = useState('')
 
-
     async function subscribe(){
-        setMessage("LOADING")
-        setError('')
-        try{
-            const response = await axios.post("/api/newsletter", { email })
-            setMessage("SUCCESS")
-        } catch(e){
-            setError(e.response.data.error)
-            setMessage("ERROR")
-        }
+      const email = document.getElementById("hero-email-input-field").value
+      const isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+      setMessage("LOADING")
+      setError('')
+      if(!email){
+        setMessage('ERROR')
+        setError("Please provide an email address")
+        return
+      }
+      if(!isValidEmail){
+        setMessage('ERROR')
+        setError("Please provide a valid email address")
+        return
+      }
+      try{
+        const response = await fetch('/api/newsletter', {
+          body: JSON.stringify({email: email}),
+          headers: {'Content-Type': 'application/json; charset=utf-8'},
+          method: 'POST',
+        });
+        const json_res = await response.json();
+        setMessage("SUCCESS")
+      } catch(e){
+        setMessage("ERROR")
+        setError(e.response.data.error)
+      }
     };
+
   return (
     <section className="text-gray-600 body-font">
   <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
@@ -30,7 +45,7 @@ export default function HeroSection(){
       <div className="flex w-full md:justify-start justify-center items-end">
         <div className="relative mr-4 md:w-full lg:w-full xl:w-1/2 w-2/4">
           <label className="leading-7 text-sm text-gray-600">Email:</label>
-          <input onChange={(e) => setEmail(e.target.value)} type="text" id="hero-field" name="hero-field" className="w-full bg-gray-100 rounded border bg-opacity-50 border-gray-300 focus:ring-2 focus:ring-indigo-200 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          <input type="text" id="hero-email-input-field" name="hero-field" className="w-full bg-gray-100 rounded border bg-opacity-50 border-gray-300 focus:ring-2 focus:ring-indigo-200 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
         </div>
         <button onClick={subscribe} className={`inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg ${
             message === "LOADING" ? "button-gradient-loading" : ""
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,38 +1,33 @@
 export default async (req, res) => {
     const FORM_ID = process.env.CONVERTKIT_FORM_ID
     const API_KEY = process.env.CONVERTKIT_API_KEY
-    const BASE_URL = "https://api.covertkit.com/v3"
-        
+    const BASE_URL = process.env.CONVERTKIT_BASE_URL
+
     const { email } = req.body;
-    console.log("reztest email")
-    console.log(email)
 
     if (!email || !email.length){
-        console.log("reztest Forgot to add email")
         return res.status(400).json({
             error: "Forgot to add email?",
         });
     }
     try {
-        console.log("reztest hit addListMember");
-        const url = [BASE_URL, `forms`, FORM_ID, `subscribe`].join('/');
-        const data = {
-            api_key: API_KEY,
-            email: email,
-        }
+        const url = `${BASE_URL}forms/${FORM_ID}/subscribe`
+        const data = { email, api_key: API_KEY };
 
         const response = await fetch(url, {
             body: JSON.stringify(data),
-            headers: {'Content-Type': 'application/json; charset=utf-8'},
+            headers: {'Content-Type': 'application/json'},
             method: 'POST'
         });
-        if(response.status == 200){
-            return res.status(200).json({error : null});
+        if(response.status >= 400){
+            return res.status(400).json({
+                error: "Oops, something went wrong"
+            });
         }
+        return res.status(201).json({error : null});
     }catch(error){
-        console.log("reztest something wen wong mush")
         return res.status(400).json({
-            error: "Oops, something wen wong mush!"
+            error: error.message || error.toString()
         });
     };
 };
